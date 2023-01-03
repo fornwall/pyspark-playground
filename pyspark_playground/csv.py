dee858601024b96d004b709a1ddbab8d0becd11b
Fredrik Fornwall <fredrik@fornwall.net>
2023-01-03 14:26:14 +0100

    Add transactions

--- pyspark_playground/csv.py
+++ pyspark_playground/csv.py
@@ -4,12 +4,16 @@ from pyspark.sql import SparkSession
 def main():
     spark = SparkSession.builder.appName("MyApp").getOrCreate()
 
-    print("Reading CSV file")
+    print("Reading CSV file..")
     df = spark.read.csv("data/zipcodes.csv")
+    print("  - Printing schema")
     df.printSchema()
-
-    print("Reading CSV file - using header records for column names")
+    
+    print("\nReading CSV file - using header records for column names..")
     df = spark.read \
          .option("header", True) \
         .csv("data/zipcodes.csv")
+    print("  - Printing schema")
     df.printSchema()
+    print("  - Calling show()")
+    df.show()
---
+++ pyspark_playground/transactions.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as func
+
+# https://sparkbyexamples.com/pyspark/pyspark-read-csv-file-into-dataframe/
+def main():
+    spark = SparkSession.builder.appName("MyApp").getOrCreate()
+
+    df = spark.read \
+         .option("header", True) \
+         .option('inferSchema', True) \
+        .csv("data/transactions.csv")
+    print("  - Printing schema")
+    df.printSchema()
+    print("  - Calling show()")
+    df.show()
+    print("  - Calling groupby('UserId').avg().show()")
+    df.groupBy('UserId').avg().show()
+    print("  - Calling groupby('UserId').sum().show()")
+    df.groupBy('UserId').sum().show()
+
+    # Give alias to column - https://stackoverflow.com/questions/34394745/how-could-i-order-by-sum-within-a-dataframe-in-pyspark
+    print("  - Two topmost spenders")
+    df.groupBy('UserId') \
+        .agg(func.sum('Amount').alias('AmountSum')) \
+        .orderBy(func.col('AmountSum').desc()) \
+        .limit(2) \
+        .show()
